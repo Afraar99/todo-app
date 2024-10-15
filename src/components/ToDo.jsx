4f038a8961725b30502a0b0e84b6d2614b4094c5
Mohamed Afraar <afraararm@gmail.com>
2024-10-15 15:30:52 +0530

    Refactor ToDo component to include input box and dynamic todo list

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useRef,useState } from "react";
 import todo_icon from "../assets/todo_icon.png";
 import Todoitems from "./Todoitems";
 
 const ToDo = () => {
+  const [todoList, setTodoList] = useState([]);
+
+  const inputRef = useRef();
+
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+
+    if (inputText === "") {
+      return null;
+    }
+
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
+    };
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+  };
+
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
       {/*-------- title ----------*/}
@@ -16,19 +36,27 @@ const ToDo = () => {
 
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
         <input
+          ref={inputRef}
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
           type="text"
           placeholder="Add your task"
         />
-        <button className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">ADD +</button>
+        <button
+          onClick={add}
+          className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+        >
+          ADD +
+        </button>
       </div>
 
-       {/*-------- todo list ----------*/}
-       <div>
-        <Todoitems text="Learn Coding"/>
-        <Todoitems text="Learn React and Tailwind"/>
-       </div>
+      {/*-------- todo list ----------*/}
+      <div>
+        {todoList.map((item, index) => {
+          return <Todoitems key={index} text={item.text} id={item.id} isComplete={item.isComplete} />;
+        })}
 
+
+      </div>
     </div>
   );
 };
