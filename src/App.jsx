faf4fdd2d57fc77cbafec154b52b977084848f00
Mohamed Afraar <afraararm@gmail.com>
2024-10-15 15:30:32 +0530

    Refactor App component to use consistent quotation marks and add missing semicolon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ToDo from './components/ToDo'
+import React from "react";
+import ToDo from "./components/ToDo";
 
 const App = () => {
   return (
-    <div className='bg-stone-900 grid py-4 min-h-screen'>
+    <div className="bg-stone-900 grid py-4 min-h-screen">
       <ToDo />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
