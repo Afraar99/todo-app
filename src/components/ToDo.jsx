ce2e8ab9a67ef96f7794fff1d8c866877ee13f93
Mohamed Afraar <afraararm@gmail.com>
2024-10-15 13:59:49 +0530

    first commit

---
+++ src/components/ToDo.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import todo_icon from "../assets/todo_icon.png";
+
+const ToDo = () => {
+  return (
+    <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
+      {/*-------- title ----------*/}
+
+      <div className="flex items-center mt-7 gap-2">
+        <img className="w-8" src={todo_icon} alt="" />
+        <h1 className="text-3xl font-semibold">To Do List</h1>
+      </div>
+
+      {/*-------- input box ----------*/}
+
+        <div>
+            <input type="text" placeholder="Add your task"/>
+            <button>ADD +</button>
+        </div>
+
+    </div>
+  );
+};
+
+export default ToDo;
